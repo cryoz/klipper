9a2ba1a770518d95cc9802596610a549ba1a743c
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2024-01-22 10:10:44 -0700

    Fix missing MoveQueue in prtouch calls

--- klippy/toolhead.py
+++ klippy/toolhead.py
@@ -207,6 +207,14 @@ class MoveQueue:
         return self.lookahead_queue.lookahead()
 
 
+class MoveQueue:
+    def __init__(self):
+        self.lookahead_queue = LookAheadQueue()
+
+    def move_queue(self):
+        # Redirect the old move_queue method to the new lookahead method
+        return self.lookahead_queue.lookahead()
+
 BUFFER_TIME_LOW = 1.0
 BUFFER_TIME_HIGH = 2.0
 BUFFER_TIME_START = 0.250
