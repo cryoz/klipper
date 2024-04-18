4d441decc3431da33ce5be0f24d316eec92ef45a
CryoZ <hellion.zz@gmail.com>
2024-04-18 23:18:17 +0300

    loadcell fix K1 specific issue

--- klippy/extras/load_cell_probe.py
+++ klippy/extras/load_cell_probe.py
@@ -18,10 +18,12 @@ Q16_FRAC_BITS = (32 - (1 + Q16_INT_BITS))
 ######## Types
 
 
+
 class TapClassifierModule(object):
 
     def classify(self, tap_analysis):
         pass
+
 
 
 class NozzleCleanerModule(object):
@@ -103,6 +105,7 @@ class ForceLine(object):
 
     def to_dict(self):
         return {'slope': self.slope, 'intercept': self.intercept}
+
 
 
 #########################
@@ -304,6 +307,7 @@ TAP_BREAK_CONTACT_TOO_EARLY = ('TAP_BREAK_CONTACT_TOO_EARLY', 'Tap break-contact
 TAP_BREAK_CONTACT_TOO_LATE = ('TAP_BREAK_CONTACT_TOO_LATE', 'Tap break-contact time too late, invalid')
 TAP_PULLBACK_TOO_SHORT = ('TAP_PULLBACK_TOO_SHORT', 'Tap break-contact time too late, pullback move may be too short')
 TAP_SEGMENT_TOO_SHORT = ('TAP_SEGMENT_TOO_SHORT', 'A tap segment is too short to perform r squared calculations')
+
 
 # TODO: maybe discard points can scale with sample rate from 1 to 3
 DEFAULT_DISCARD_POINTS = 3
