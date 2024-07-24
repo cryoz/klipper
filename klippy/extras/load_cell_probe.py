ce1e34fb61d15433d28c0f1166dc816643364443
Blinov Evgeny <bl1n0v3vg3ny@gmail.com>
2024-07-24 23:47:53 +0300

    disable logging trapq move

--- klippy/extras/load_cell_probe.py
+++ klippy/extras/load_cell_probe.py
@@ -403,7 +403,7 @@ class TapAnalysis(object):
         for move in moves:
             moves_out.append(TrapezoidalMove(move))
             # DEBUG: enable to see trapq contents
-            logging.info("trapq move: %s" % (moves_out[-1].to_dict(),))
+            # logging.info("trapq move: %s" % (moves_out[-1].to_dict(),))
         num_moves = len(moves_out)
         if num_moves < 5 or num_moves > 7:
             raise self.printer.command_error(
